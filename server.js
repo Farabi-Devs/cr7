34f879dbf2de9ef2272e3493098e78bcfcf2117d
Farabi <ariyanfarabiehan2@gmail.com>
2025-10-07 16:35:26 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 import express from "express";
-import helpdiscord from "./helpdiscord.js";
 import defaultRoute from "./default.js";
+import helpdiscord from "./helpdiscord.js";
 import fallback from "./fallback.js";
 
 const app = express();
 
-// handle /help?discord first
 app.use(helpdiscord);
-
-// handle /[PROMPT] ai routes
 app.use(defaultRoute);
-
-// handle empty root /
 app.use(fallback);
 
 const port = process.env.PORT || 3000;
