2738cb7aeb2fc69f1af2a32af402289df4ec6515
Farabi <ariyanfarabiehan2@gmail.com>
2025-10-07 16:38:28 +0600

    Update helpdiscord.js

--- helpdiscord.js
+++ helpdiscord.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const router = express.Router();
+import express from "express";
+const app = express();
 
-router.get('/help', (req, res) => {
-    if (req.query.discord !== undefined) {
-        const discordlink = 'https://discord.gg/69';
-        try {
-            res.redirect(discordlink);
-        } catch {
-            res.send(`You can visit Discord here: <a href="${discordlink}">${discordlink}</a>`);
-        }
-    } else {
-        res.send('no valid query provided');
+app.get("/help", (req, res) => {
+  if (req.query.discord !== undefined) {
+    const discordlink = 'https://discord.gg/69';
+    try {
+      res.redirect(discordlink);
+    } catch {
+      res.send(`You can visit Discord here: <a href="${discordlink}">${discordlink}</a>`);
     }
+  } else {
+    res.send('no valid query provided');
+  }
 });
 
-module.exports = router;
+export default app;
